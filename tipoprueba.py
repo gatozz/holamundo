08a839144e28d5d21327c3f8b9f228d31e6088c5
gatozz <di.benavidesa@duocuc.cl>
2024-06-12 11:12:52 -0400

    menu prueba

---
+++ tipoprueba.py
@@ -0,0 +1,18 @@
+while True:
+    print("")
+    print("~~~~~~Menu~~~~~~")
+    print("")
+    print("1. Agregar productos")
+    print("2. Lista de todos los productos")
+    print("3. Eliminar producto por ID")
+    print("4. Generar archivo csv")
+    print("5. Cargar archivo csv")
+    print("6. Estadistica")
+    print("0. Salir")
+    print("")
+    print("~~~~~~~~~~~~~~~")
+    print("")
+    op=int(input("Ingrese una opcion : \n"))
+    
+    if op==1:
+        print("")
